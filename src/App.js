112fe86962263745a4b7aab6212dbf990d6f9009
Jesus <chuygs12@gmail.com>
2023-01-07 11:24:26 -0600

    add some styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import Note from './components/Note'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import noteService from './services/notes'
+import Notification from './components/Notification' 
+import Footer from './components/Footer'
 
 const App = () => {
   
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState("")
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState("some error happened...")
 
   useEffect(() => {
     noteService
@@ -50,9 +53,13 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(
-          `the note '${note.content}' was already deleted from server`
+        setErrorMessage(
+          `Note ${note.content} was already in the server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -60,6 +67,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important": "all"}  
@@ -81,6 +89,7 @@ const App = () => {
             />
           <button type='submit'>Save</button>
       </form>
+      <Footer/>
     </div>
   )
 }
