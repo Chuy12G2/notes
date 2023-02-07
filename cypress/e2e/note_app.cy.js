841277d53697b07d634863b3b8d4390a32e7c21a
Jesus <chuygs12@gmail.com>
2023-02-06 19:21:09 -0600

    create cypress tests

---
+++ cypress/e2e/note_app.cy.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-undef */
+describe('Note app', function() {
+  beforeEach(function(){
+    cy.visit('http://localhost:3000')
+  })
+  
+  it('front page can be opened', function() {
+    cy.contains('Notes')
+    cy.contains('Note app, Department of Computer Science, University of Helsinki 2022')
+  })
+
+  it('login form can be opened', function() {
+    cy.contains('log in').click()
+  })
+
+  it('user can log in', function() {
+    cy.contains('log in').click()
+    cy.get('#username').type('user1')
+    cy.get('#password').type('password')
+    cy.get('#login-button').click()
+
+    cy.contains('Usuario de prueba logged in')
+  })
+
+  describe('when logged in', function() {
+    beforeEach(function() {
+      cy.contains('log in').click()
+      cy.get('input:first').type('user1')
+      cy.get('input:last').type('password')
+      cy.get('#login-button').click()
+      cy.get('#login-button').click()
+    })
+
+    it('a new note can be created', function() {
+      cy.contains('new note').click()
+      cy.get('input').type('a note created by cypress')
+      cy.contains('save').click()
+      cy.contains('a note created by cypress')
+    })
+  })
+
+})
--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
+import { useState, useEffect, useRef } from 'react'
 import Note from './components/Note'
-import { useState, useEffect } from 'react'
-import noteService from './services/notes'
-import Notification from './components/Notification' 
+import Notification from './components/Notification'
 import Footer from './components/Footer'
+import LoginForm from './components/LoginForm'
+import NoteForm from './components/NoteForm'
+import Togglable from './components/Togglable'
+import noteService from './services/notes'
 import loginService from './services/login'
 
 const App = () => {
-  
   const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState("")
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
-  const [username, setUsername] = useState('')
+
+  const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
 
@@ -23,139 +25,111 @@ const App = () => {
       })
   }, [])
 
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() > 0.5
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
     }
+  }, [])
 
+  const noteFormRef = useRef()
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      noteService.setToken(user.token)
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      ) 
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const addNote = (noteObject) => {
     noteService
       .create(noteObject)
       .then(returnedNote => {
         setNotes(notes.concat(returnedNote))
-        setNewNote('')
+        noteFormRef.current.toggleVisibility()
       })
   }
 
-  const handleNoteChange = (event) => {
-    setNewNote(event.target.value)
-  }
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
 
-  
-
-
-
-  const notesToShow = showAll ? notes : notes.filter(note => note.important)
-  
-  
-  const toggleImportanceOf = id => {
-    const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important }
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id);
+    // eslint-disable-next-line indent
+    const changedNote = { ...note, important: !note.important };
 
     noteService
       .update(id, changedNote)
-      .then(returnedNote => {
-        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
       })
-      .catch(error => {
+      .catch(() => {
         setErrorMessage(
-          `Note ${note.content} was already in the server`
-        )
+          `Note '${note.content}' was already removed from server`
+        );
         setTimeout(() => {
-          setErrorMessage(null)
-        }, 5000)
-
-        setNotes(notes.filter(n => n.id !== id))
-      })
-  }
-
-  const handleLogin = async (event) => {
-    event.preventDefault()
-    try{
-      const user = await loginService.login({
-        username, password
-      })
-      noteService.setToken(user.token)
-      setUser(user)
-      setUsername('')
-      setPassword('')
-    } catch(exception){
-      setErrorMessage('Wrong credentials')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000);
-    }
-  }
-
-  const loginForm = () => {
-    return (
-    <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input
-          type='text'
-          value={username}
-          name='Username'
-          onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-          <input
-          type='password'
-          value={password}
-          name='Password'
-          onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type='submit'>login</button>
-      </form>
-    )
-  }
-
-  const noteForm = () => {
-    return(
-    <form onSubmit={addNote}>
-          <input 
-            value={newNote}
-            onChange={handleNoteChange}
-            />
-          <button type='submit'>Save</button>
-      </form>
-    )
-  }
-
+          setErrorMessage(null);
+        }, 5000);
+        setNotes(notes.filter((n) => n.id !== id))
+      })}
 
   return (
     <div>
-      <h1>Notes</h1>
-      <Notification message={errorMessage}/>
-      
-      {!user && loginForm()}
-      {user && <div>
-        <p>{user.name} logged in</p>
-        {noteForm()}
+      <h1>Notes app</h1>
+      <Notification message={errorMessage} />
+
+      {!user &&
+        <Togglable buttonLabel="log in">
+          <LoginForm
+            username={username}
+            password={password}
+            handleUsernameChange={({ target }) => setUsername(target.value)}
+            handlePasswordChange={({ target }) => setPassword(target.value)}
+            handleSubmit={handleLogin}
+          />
+        </Togglable>
+      }
+      {user &&
+        <div>
+          <p>{user.name} logged in</p>
+          <Togglable buttonLabel="new note" ref={noteFormRef}>
+            <NoteForm createNote={addNote} />
+          </Togglable>
         </div>
       }
-      
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? "important": "all"}  
+          show {showAll ? 'important' : 'all' }
         </button>
-      </div>
+      </div> 
       <ul>
         {notesToShow.map(note => 
-          <Note 
-            key={note.id} 
+          <Note
+            key={note.id}
             note={note}
             toggleImportance={() => toggleImportanceOf(note.id)}
           />
         )}
       </ul>
-      
-      <Footer/>
+
+      <Footer />
     </div>
   )
 }
